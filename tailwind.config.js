243a97b59381da0b89e0eb8a98108c6facf7dfab
Andrija Jelenkovic <andrija.jelenkovic@icentic.com>
2021-11-12 12:15:44 +0100

    Change tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./dist/*.html'],
+  purge: ['./dist/*.html', './src/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
